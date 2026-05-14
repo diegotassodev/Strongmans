fe67196353c00478554666f91d715a88d5bee60f
Diego Tasso <108370404+diegotassodev@users.noreply.github.com>
2026-05-14 17:02:42 -0300

    fix: corrigir edição de usuários no menu administrativo

--- Menus/MenuAdmin.cs
+++ Menus/MenuAdmin.cs
@@ -5,8 +5,9 @@ internal class MenuAdmin : Menu {
         ExibirTitulo("ADMINISTRADOR");
         Console.WriteLine ("1. Adicionar Usuário");
         Console.WriteLine ("2. Remover Usuário");
-        Console.WriteLine ("3. Mostrar Usuários");
-        Console.WriteLine ("4. Entrar Menu Principal");
+        Console.WriteLine ("3. Editar Usuário");
+        Console.WriteLine ("4. Mostrar Usuários");
+        Console.WriteLine ("5. Entrar Menu Principal");
         Console.WriteLine ("0. Sair");
         int opcao = EntradaOpcaoUsuario();
         ProcessandoOpcaoUsuario(opcao);
@@ -26,8 +27,9 @@ internal class MenuAdmin : Menu {
         switch (opcao) {
             case 1: MenuAdicionarUsuario.Executar(); RetornandoTelaAdmin(); break;
             case 2: MenuRemoverUsuario.Executar(); RetornandoTelaAdmin(); break;
-            case 3: MenuExibirUsuarios.Executar(); RetornandoTelaAdmin(); break;
-            case 4: MenuPrincipal.Executar(); break;
+            case 3: MenuEditarUsuarios.Executar(); RetornandoTelaAdmin(); break;
+            case 4: MenuExibirUsuarios.Executar(); RetornandoTelaAdmin(); break;
+            case 5: MenuPrincipal.Executar(); break;
             case 0: Console.WriteLine ("Encerrando o Programa."); break;
             default: RetornandoTelaAdmin(); break;
         }
--- Menus/MenuUsuario/MenuEditarUsuario.cs
+++ Menus/MenuUsuario/MenuEditarUsuario.cs
@@ -6,42 +6,43 @@ internal class MenuEditarUsuarios : Menu {
     public static void Executar() {
         ExibirTitulo("Editar Usuários");
         Usuario.listaUsuarios.ForEach(usuario => Console.WriteLine($"[{usuario.Id}] {usuario.Nome}"));
-        Console.Write("Selecione o id do usuário que você deseja editar: ");
+        Console.Write("Selecione o ID do usuário que você deseja editar: ");
         int idUsuario; int.TryParse(Console.ReadLine()!, out idUsuario);
 
         if (Usuario.listaUsuarios.Any(u => u.Id == idUsuario)) {
             Usuario usuario = Usuario.listaUsuarios.First(u => u.Id == idUsuario);
             ExibirTitulo(usuario.Nome!);
             Console.WriteLine($"1. Nome: {usuario.Nome}");
-            Console.WriteLine($"2. Email: {usuario.Email}m");
-            Console.WriteLine($"3. Senha: {usuario.Senha}kg");
+            Console.WriteLine($"2. Email: {usuario.Email}");
+            Console.WriteLine($"3. Senha: {usuario.Senha}");
             Console.Write("Selecione o que deseja editar: ");
             int opcao; int.TryParse(Console.ReadLine()!, out opcao);
 
             switch (opcao) {
-                case 1: EditarNome(usuario); usuario.SerializarUsuario(); RetornandoTelaPrincipal(); break;
+                case 1: EditarNome(usuario); usuario.SerializarUsuario(); RetornandoTelaAdmin(); break;
                 case 2: EditarEmail(usuario); usuario.SerializarUsuario(); RetornandoTelaAdmin(); break;
+                case 3: EditarSenha(usuario); usuario.SerializarUsuario(); RetornandoTelaAdmin(); break;
                 default: Console.WriteLine("Processo cancelado."); RetornandoTelaAdmin(); break;
             }
         }
     }
 
     public static void EditarNome(Usuario usuario) {
-        Console.WriteLine("Digite um novo nome para o usuário: ");
+        Console.Write("Digite um novo nome para o usuário: ");
         string nomeUsuario = Console.ReadLine()!;
         usuario.Nome = nomeUsuario;
         Console.WriteLine($"Sucesso na operação de troca de nome para: {nomeUsuario}!");
     }
 
     public static void EditarEmail(Usuario usuario) {
-        Console.WriteLine("Digite um novo email para o usuário: ");
+        Console.Write("Digite um novo email para o usuário: ");
         string emailUsuario = Console.ReadLine()!;
         usuario.Email = emailUsuario;
         Console.WriteLine($"Sucesso na operação de troca de email para: {emailUsuario}!");
     }
 
     public static void EditarSenha(Usuario usuario) {
-        Console.WriteLine("Digite uma nova senha para o usuário: ");
+        Console.Write("Digite uma nova senha para o usuário: ");
         string senhaUsuario = Console.ReadLine()!;
         usuario.Senha = senhaUsuario;
         Console.WriteLine($"Sucesso na operação de troca de senha para: {senhaUsuario}!");
